0e16b41790b22f92e96d2046bbcf8675900a9db1
Vera Yu <verayu217@gmail.com>
2022-01-15 13:21:36 +0800

    新增用日期套件取得今天日期

--- crawler/crawler3.js
+++ crawler/crawler3.js
@@ -1,15 +1,18 @@
 const axios = require("axios");
-const { readFile } = require("fs/promises");
 
+// 得到讀取檔案的函式(nodejs內建)
+const { readFile } = require("fs/promises");
+// 得到取得日期套件(第三方所以要安裝、引用)
+const moment = require("moment");
 
 
 (async () => {
   try {
-   
 
     // 根據變數去抓取資料
     let stockNo = await readFile("stock.txt", "utf-8");
-    let queryDate = "20220115";
+    let queryDate = moment().format("YYYYMMDD"); // 自動用今天的日期 不寫死
+
 
     // let response = await axios.get(
     //   `https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=${queryDate}&stockNo=${stockNo}`
